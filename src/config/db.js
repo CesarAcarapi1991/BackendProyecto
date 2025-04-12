7e8651f37110819c5dc6c76d380bea9de2c181b0
cesar.acarapi <cesar21908@gmail.com>
2025-04-12 01:15:12 -0400

    solucionando db.js para el certificado

--- src/config/db.js
+++ src/config/db.js
@@ -12,7 +12,7 @@ const sequelize = new Sequelize(
     dialectOptions:{
       ssl:{
         require: true,
-        rejectUnathorized: false
+        rejectUnauthorized: false
       }
     }
   }
